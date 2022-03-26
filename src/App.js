42a580a52230faa6327e9f42901981ab9a3fa545
Klim <klim_somov@mail.ru>
2022-03-26 11:12:30 +0300

    trying to implement auto message from BOT

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import { useEffect, useState } from "react";
 import Form from "./components/form/Form";
 
 
-const msgs = [];
+
 
 function App() {
-  const [messages, setMessages] = useState(msgs);
-
+  const [messages, setMessages] = useState([]);
+ 
+  useEffect(() => {
+      setTimeout(() => {
+      setMessages([...messages, { text: "want to talk?", author: " BOT", id: Math.random()}]);
+    }, 2000);
+    return () => {
+      console.log('ee')
+    }
+  
+  }, [])
+  
   const addMessage = (newText) => {
     setMessages([...messages, { text: newText, author: " Klim Somov", id: Math.random()  }]);
   };
@@ -19,6 +29,7 @@ function App() {
       <header className="App-header">
         <Form onSubmit={addMessage} />
         {messages.map((m) => (<Message key = {m.id} text={m.text} author={m.author}/>))}
+        
       </header>
     </div>
   );
